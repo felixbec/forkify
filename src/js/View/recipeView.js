fb1d145e8baffea0fb508b7888925a2fed7d33fb
Carlos <cfelixbeltran@dynamicagency.com>
2020-05-03 12:56:25 -0500

    ADDED recipe liked functionality to RecipeView

--- src/js/View/recipeView.js
+++ src/js/View/recipeView.js
@@ -36,7 +36,7 @@ export const clearRecipe = () => {
     elements.recipe.innerHTML = '';
 };
 
-export const renderRecipe = recipe => {
+export const renderRecipe = (recipe, isLiked) => {
         const markup = `
         <figure class="recipe__fig">
             <img src="${recipe.img}" alt="${recipe.title}" class="recipe__img">
@@ -75,7 +75,7 @@ export const renderRecipe = recipe => {
             </div>
             <button class="recipe__love">
                 <svg class="header__likes">
-                    <use href="img/icons.svg#icon-heart-outlined"></use>
+                    <use href="img/icons.svg#icon-heart${isLiked ? '' : '-outlined'}"></use>
                 </svg>
             </button>
         </div>
@@ -85,7 +85,7 @@ export const renderRecipe = recipe => {
                 ${recipe.ingredients.map(el => createIngredient(el)).join(' ')}
             </ul>
 
-            <button class="btn-small recipe__btn">
+            <button class="btn-small recipe__btn recipe__btn--add">
                 <svg class="search__icon">
                     <use href="img/icons.svg#icon-shopping-cart"></use>
                 </svg>
